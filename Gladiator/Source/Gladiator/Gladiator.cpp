b35f11aedff0d9b5f943b087461f85c8f9e80867
Umut OSMANOGLU <u.osmanoglu@student.isartdigital.com>
2022-10-24 14:25:13 +0200

    Add Project

---
+++ Gladiator/Source/Gladiator/Gladiator.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Gladiator.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Gladiator, "Gladiator" );
---
+++ Gladiator/Source/Gladiator/GladiatorGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GladiatorGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GLADIATOR_API AGladiatorGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
