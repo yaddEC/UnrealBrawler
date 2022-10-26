77e0d0dc12bcff6be6557fabcd09e5221648a530
Yann DECHAUX <y.dechaux@student.isartdigital.com>
2022-10-26 10:31:26 +0200

    Player Movment fixed

--- Gladiator/Source/Gladiator/Private/PlayerGladiator.cpp
+++ Gladiator/Source/Gladiator/Private/PlayerGladiator.cpp
@@ -2,39 +2,57 @@
 
 
 #include "PlayerGladiator.h"
+#include "HeadMountedDisplayFunctionLibrary.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/InputComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/Controller.h"
+#include "GameFramework/SpringArmComponent.h"
 
 // Sets default values
 APlayerGladiator::APlayerGladiator()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
 
-}
 
-// Called when the game starts or when spawned
-void APlayerGladiator::BeginPlay()
-{
-	Super::BeginPlay();
+	BaseTurnRate = 45.f;
+	BaseLookUpRate = 45.f;
+
 	
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	
+	GetCharacterMovement()->bOrientRotationToMovement = true;	
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f); 
+	GetCharacterMovement()->JumpZVelocity = 600.f;
+	GetCharacterMovement()->AirControl = 0.2f;
+
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->TargetArmLength = 300.0f; 	
+	CameraBoom->bUsePawnControlRotation = true; 
+
+	
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
 }
 
-// Called every frame
-void APlayerGladiator::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
 
-}
 
-// Called to bind functionality to input
 void APlayerGladiator::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-	PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &APlayerGladiator::MoveForward);
-	PlayerInputComponent->BindAxis(TEXT("MoveRight"), this, &APlayerGladiator::MoveRight);
-	PlayerInputComponent->BindAxis(TEXT("Turn"), this, &APlayerGladiator::AddControllerYawInput);
-	PlayerInputComponent->BindAxis(TEXT("LookUp"), this, &APlayerGladiator::AddControllerPitchInput);
-	PlayerInputComponent->BindAction(TEXT("Jump"), IE_Pressed, this, &APlayerGladiator::Jump);
+	check(PlayerInputComponent);
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
+	PlayerInputComponent->BindAxis("MoveForward", this, &APlayerGladiator::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &APlayerGladiator::MoveRight);
+	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
 }
+
 
 void APlayerGladiator::MoveForward(float Axis)
 {
